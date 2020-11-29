465f77fcf9a0c013c53957b0150b2f9e01640df3
Atharvat77 <Atharvat77@gmail.com>
2020-11-29 20:40:41 +0530

    version 3.3.1

--- Fake News Detection/User Screen/userHomePage.js
+++ Fake News Detection/User Screen/userHomePage.js
@@ -7,7 +7,7 @@
     
     function isElementInViewport(item) {
         var rect = item.getBoundingClientRect();
-        console.log(rect);
+        
         
         return (
           rect.top >= outerDiv.top &&
@@ -28,3 +28,17 @@
       outer.addEventListener("scroll", callbackFunc);
     
     })();
+
+var headlines=["h1","h2","h3","h4","h5","h6","h7","h8","h9","h10","h11","h12","h13","h14","h15","h16","h17","h18","h19","h20","h21","h22","h23","h24","h25","h26","h27","h28","h29","h30"]
+var news=["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15","c16","c17","c18","c19","c20","c21","c22","c23","c24","c25","c26","c27","c28","c29","c30"]
+
+var newsItems=document.querySelectorAll(".timeline ul li div p");
+console.log(newsItems.length+" asdasd");
+for(var i=0;i<newsItems.length;i++)
+{
+  newsItems[i].innerHTML=news[i];
+}
+
+
+
+
